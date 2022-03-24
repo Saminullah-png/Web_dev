b0eab4771bc01b67e4d7283de8914817157f812b
Saminullah-png <samin_azrakyno1@yahoo.com>
2022-03-24 23:09:34 +0530

    proved that var let and const are behaving different in hoisting, dec, and initialization

--- Modeul_1/hoisting.js
+++ Modeul_1/hoisting.js
@@ -1,2 +1,23 @@
-var a = 10;
+// var a;
+
+// console.log(a);
+// a = 11;
+//undefined result for redeclation 
+
+// let b;
+// console.log(b);
+// b = 1;
+
 console.log(a);
+var a = 10;
+// console.log(a);
+
+
+let b = 1;
+console.log(b);
+
+// const c;
+// c = 3;
+// console.log(c);
+const c = 3;
+console.log(c);
