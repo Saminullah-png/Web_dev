24f95dd62b9ce671e1e47d28530f1763398a1549
Saminullah-png <samin_azrakyno1@yahoo.com>
2022-03-24 23:50:27 +0530

    file path from module 1 to module 2

---
+++ Module_2/interview/excecutioncontext.js
@@ -0,0 +1,22 @@
+// let a = "hello";
+
+// console.log(a);
+
+// console.log('hi');
+
+// var a = 10;
+
+// function fun() {
+//     var b = 20;
+//     console.log(20);
+// }
+// console.log(a);
+
+// var c = 20;
+// fun();
+
+// function();
+var fun = function() {
+    console.log("Hello ");
+}
+fun();
