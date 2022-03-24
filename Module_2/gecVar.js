add4b641b55a77735d2d5c72d1ea285981ea0008
Saminullah-png <samin_azrakyno1@yahoo.com>
2022-03-24 23:47:31 +0530

    finished and scope will be in another day

---
+++ Module_2/gecVar.js
@@ -0,0 +1,41 @@
+// var a;
+
+// console.log(a);
+// a = 11;
+//undefined result for redeclation 
+
+// let b;
+// console.log(b);
+// b = 1;
+
+console.log(a);
+var a = 10;
+// console.log(a);
+
+
+let b = 1;
+console.log(b);
+
+// const c;
+// c = 3;
+// console.log(c);
+const c = 3;
+console.log(c);
+
+
+//hoisting
+// console.log(a);
+// var a = 10;
+
+// console.log(a);
+// var a;
+// a = 10;
+// console.log(a);
+
+// a = 20;
+
+// console.log(a);
+
+
+const a = 20;
+console.log(a);
